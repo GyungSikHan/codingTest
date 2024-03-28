e5eb838d2419888b66a42e04f2ecfd6e7e95a844
GyungSik Han <gksrudtlr2@icloud.com>
2024-03-28 18:41:57 +0900

    최소신장트리

--- Test/Back_1197/Back_1197.cpp
+++ Test/Back_1197/Back_1197.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+
+bool Find(vector<int> un, int fi, int se)
+{
+    int temp = un[fi];
+
+    if(un[temp] == un[se])
+        return false;
+
+    return true;
+}
 
 int main()
 {
@@ -8,7 +20,7 @@ int main()
     int v{}, e{};
     cin>>v>>e;
 
-    vector<pair<int, pair<int, int>> edgeList;
+    vector<pair<int, pair<int, int>>> edgeList;
     vector<int> un(v+1,0);
 
     for(int i = 1; i <= v; i++)
@@ -20,5 +32,18 @@ int main()
         edgeList.push_back({c,{a,b}});
     }
 
+    sort(edgeList.begin(),edgeList.end());
+
+    for (int i = 0; i < e; i++)
+    {
+        if(Find(un, edgeList[i].second.first, edgeList[i].second.second) == true)
+        {
+            un[edgeList[i].second.first] = edgeList[i].second.second;
+        }
+    }
+
+
+
+    int n{};
 
 }
