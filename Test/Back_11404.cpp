0689ad482e076b521c3cf0ceb68acfde10d4af23
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2024-03-28 07:59:30 +0000

    플로이드워셜

--- Test/Back_11404.cpp
+++ Test/Back_11404.cpp
@@ -1,3 +1,48 @@
 #include <iostream>
+#include <vector>
+#include<climits>
+using namespace std;
 
-using namesp
+int INF = 1e9;
+int a, b, c;
+
+int main()
+{
+    int n{},m{};
+    cin>>n;
+    cin>>m;
+
+    vector<vector<int>> rod(n+1,vector<int>(n+1, INF));
+
+    for (int i = 1; i <= n; i++)
+        rod[i][i] = 0;
+    
+    for (int i = 0; i < m; i++)
+    {
+        cin >>a >> b >> c;
+        rod[a][b] = min(rod[a][b], c);
+    }
+
+    for(int k = 1; k <= n; k++)
+    {
+        for (int s = 1; s <= n; s++)        
+        {
+            for (int e = 1; e <= n; e++)
+            {
+                rod[s][e] = min(rod[s][e], rod[s][k] + rod[k][e]);
+            }
+        }       
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            if(rod[i][j] == INF)
+                cout<<0<<" ";
+            else
+                cout<<rod[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
