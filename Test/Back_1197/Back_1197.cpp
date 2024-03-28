d4ab5b541b36ee9a878b41ca01d656943bbd2d0d
GyungSik Han <gksrudtlr2@icloud.com>
2024-03-29 01:36:02 +0900

    최소신장트리

--- Test/Back_1197/Back_1197.cpp
+++ Test/Back_1197/Back_1197.cpp
@@ -4,14 +4,22 @@
 using namespace std;
 
 
-bool Find(vector<int> un, int fi, int se)
+int Find(vector<int>& un, int data)
 {
-    int temp = un[fi];
+    if(un[data] == data)
+        return data;
+    return un[data] = Find(un, un[data]);
+}
 
-    if(un[temp] == un[se])
-        return false;
+void Union(vector<int>& un, int first, int second)
+{
+    first = Find(un,first);
+    second = Find(un, second);
 
-    return true;
+    if(first != second)
+        un[first] = second;
+    else
+        un[second] = first;
 }
 
 int main()
@@ -34,16 +42,16 @@ int main()
 
     sort(edgeList.begin(),edgeList.end());
 
+    int sum{};
+
     for (int i = 0; i < e; i++)
     {
-        if(Find(un, edgeList[i].second.first, edgeList[i].second.second) == true)
+        if(Find(un, edgeList[i].second.first) != Find(un, edgeList[i].second.second))
         {
-            un[edgeList[i].second.first] = edgeList[i].second.second;
+            Union(un, edgeList[i].second.first, edgeList[i].second.second);
+            sum += edgeList[i].first;
         }
     }
 
-
-
-    int n{};
-
+    cout<<sum<<endl;
 }
