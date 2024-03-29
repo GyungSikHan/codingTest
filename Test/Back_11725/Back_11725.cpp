42c0713019077a7a29352e9e3a0aad1ba2686461
GyungSik Han <gksrudtlr2@icloud.com>
2024-03-30 02:56:56 +0900

    트리의 부모찾기

--- Test/Back_11725/Back_11725.cpp
+++ Test/Back_11725/Back_11725.cpp
@@ -1,20 +1,57 @@
-﻿// Back_11725.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <vector>
+#include <queue>
 
-#include <iostream>
+using namespace std;
+
+int n{};
+int a{}, b{};
+vector<vector<int>> Tree;
+vector<int> Parent;
+
+void Bfs()
+{
+	queue<int> q;
+	q.push(1);
+	Parent[1] = 0;
+
+	while (q.empty() == false)
+	{
+		int curr = q.front();
+		q.pop();
+
+		for (int i : Tree[curr])
+		{
+			if (Parent[i] == -1)
+			{
+				Parent[i] = curr;
+				q.push(i);
+			}
+		}
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	Tree.resize(n + 1);
+	Parent.resize(n + 1, -1);
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		cin >> a >> b;
+		Tree[a].push_back(b);
+		Tree[b].push_back(a);
+	}
+
+	Bfs();
+
+	for (int i = 2; i <= n; i++)
+	{
+		cout << Parent[i] << "\n";
+	}
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
