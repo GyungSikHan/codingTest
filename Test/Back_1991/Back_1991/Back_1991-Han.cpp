0af4b192c1d2fe35ce07e706eeef38e15c7da267
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-01 19:09:00 +0900

    트리

---
+++ Test/Back_1991/Back_1991/Back_1991-Han.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n;
+char p;
+char l;
+char r;
+char node[27][2];
+
+void Preorder(char node[27][2], int i)
+{
+
+	cout << char(i+64);
+
+	if(node[i][0] != '.')
+		Preorder(node, i * 2);
+	if(node[i][1] != '.')
+		Preorder(node,  i * 2 + 1);
+}
+
+int main()
+{
+    cin>>n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> p >> l >> r;
+		node[i][0] = l;
+		node[i][1] = r;
+	}
+
+	Preorder(node, 1);
+	for (int i = 1; i <= n; i++)
+	{
+		cout << char(i + 64) << " " << node[i][0] << " " << node[i][1] << endl;
+	}
+}
