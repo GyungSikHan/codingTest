f584fc209c3ea0348fed80061fbd222ff69a1a34
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-15 14:00:54 +0900

    LCA2-최소공통부모 빠르게 구하기

--- Test/Back_11438/Back_11438/Back_11438.cpp
+++ Test/Back_11438/Back_11438/Back_11438.cpp
@@ -1,32 +1,83 @@
-﻿#include <iostream>
+﻿#include <cstdio>
 #include <vector>
-#include <queue>
+#include <cmath>
 
 using namespace std;
 
-int n, m;
-int kmax;
-int parent[21][100001];
-vector<int> Depth;
-vector<bool> Visit;
+const int MAXN = 100001;
+const int MAXK = 17; // log2(MAXN) + 1
+
 vector<vector<int>> Tree;
+vector<int> Depth;
+vector<vector<int>> Parent;
 
-int main()
-{
-	int a{}, b{};
-	cin >> n;
+void DFS(int node, int parent, int depth) {
+    Depth[node] = depth;
+    Parent[node][0] = parent;
 
-	Tree.resize(n + 1);
-	Depth.resize(n + 1, 0);
-	Visit.resize(n + 1, false);
+    for (int i = 1; i < MAXK; ++i) {
+        int prev_parent = Parent[node][i - 1];
+        Parent[node][i] = Parent[prev_parent][i - 1];
+    }
 
-	for (int i = 0; i < n - 1; i++)
-	{
-		cin >> a >> b;
-		Tree[a].push_back(b);
-		Tree[b].push_back(a);
-	}
+    for (int child : Tree[node]) {
+        if (child != parent) {
+            DFS(child, node, depth + 1);
+        }
+    }
+}
 
-	//n이 노드의 개수
+int LCA(int a, int b) {
+    if (Depth[a] < Depth[b])
+        swap(a, b);
 
+    int diff = Depth[a] - Depth[b];
+
+    for (int i = 0; i < MAXK; ++i) {
+        if (diff & (1 << i)) 
+        {
+            a = Parent[a][i];
+        }
+    }
+
+    if (a == b)
+        return a;
+
+    for (int i = MAXK - 1; i >= 0; --i) {
+        if (Parent[a][i] != Parent[b][i]) {
+            a = Parent[a][i];
+            b = Parent[b][i];
+        }
+    }
+
+    return Parent[a][0];
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    Tree.resize(n + 1);
+    Depth.resize(n + 1);
+    Parent.resize(n + 1, vector<int>(MAXK));
+
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        Tree[u].push_back(v);
+        Tree[v].push_back(u);
+    }
+
+    DFS(1, 0, 0);
+
+    int m;
+    scanf("%d", &m);
+
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        printf("%d\n", LCA(u, v));
+    }
+
+    return 0;
 }
