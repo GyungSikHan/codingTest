bacf7b178549c1561b4576947fec012c9ce0a64b
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-22 17:30:58 +0900

    deque를 이용한 queuestack 문제

---
+++ Test/Back_24511/Back_24511/Back_24511.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+deque<int> de;
+int n{};
+int m{};
+int a{};
+bool flag[100001];
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> flag[i];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> m;
+		if (flag[i] == 0)
+			de.push_back(m);
+	}
+
+	cin >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> a;
+		de.push_front(a);
+		cout << de.back() << " ";
+		de.pop_back();
+	}
+}
