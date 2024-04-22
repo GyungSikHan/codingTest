910e81c87f3654df7e6d791414b8fcf074a2af23
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-22 15:48:25 +0900

    데큐문제

---
+++ Test/Back_28279/Back_28279/Back_28279.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <deque>
+
+using namespace std;
+
+deque<int> q;
+int n{};
+int m{};
+int a{};
+
+int main()
+{
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+
+	cin >> n;
+
+	while(n--)
+	{
+		cin >> m;
+
+		switch (m)
+		{
+		case 1:
+			cin >> a;
+			q.push_front(a);
+			break;
+		case 2:
+			cin >> a;
+			q.push_back(a);
+			break;
+		case 3:
+			if (q.empty() == false)
+			{
+				cout << q.front() << "\n";
+				q.pop_front();
+				break;
+			}
+			cout << -1 << "\n";
+			break;
+		case 4:
+			if (q.empty() == false)
+			{
+				cout << q.back() << "\n";
+				q.pop_back();
+				break;
+			}
+				cout << -1 << "\n";
+			break;
+		case 5:
+			cout << q.size() << "\n";
+			break;
+		case 6:
+			if (q.empty() == true)
+			{
+				cout << 1 << "\n";
+				break;
+			}
+			cout << 0 << "\n";
+			break;
+		case 7:
+			if (q.empty() == false)
+			{
+				cout << q.front() << "\n";
+				break;
+			}
+			cout << -1 << "\n";
+			break;
+		case 8:
+			if (q.empty() == false)
+			{
+				cout << q.back() << "\n";
+				break;
+			}
+			cout << -1 << "\n";
+			break;
+		}
+	}
+}
