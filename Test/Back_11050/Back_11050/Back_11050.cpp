05081ded71c3ac5f452fdb4e19f398c61c283a42
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-23 14:05:16 +0900

    이항계수1

---
+++ Test/Back_11050/Back_11050/Back_11050.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Factorial(int a)
+{
+	int data = 1;
+	for (int i = 1; i <= a; i++)
+		data *= i;
+
+	return data;
+}
+
+int main()
+{
+	int n{}, k{};
+	cin >> n >> k;
+	int result = Factorial(n) / (Factorial(n - k) * Factorial(k));
+
+	cout << result << endl;
+}
