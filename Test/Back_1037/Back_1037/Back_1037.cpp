e62b1410829c7eee7fed94cc4880393aa372fe9e
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-23 14:43:57 +0900

    약수

---
+++ Test/Back_1037/Back_1037/Back_1037.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n{}, a{};
+	long long min = 1000001, max = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+
+		if (max < a)
+			max = a;
+		if (min > a)
+			min = a;
+	}
+
+	cout << min * max << endl;
+}
