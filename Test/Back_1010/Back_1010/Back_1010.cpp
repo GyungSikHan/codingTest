10ca22bf866c9c9229e44c4454470c2839be8c8b
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-23 14:32:54 +0900

    다리놓기

---
+++ Test/Back_1010/Back_1010/Back_1010.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int Solution(int n, int m)
+{
+	int a = 1, b = 1;
+
+	for (int i = m; i > m-n; i--)
+	{
+		a *= i;
+		a /= b;
+		b++;
+	}
+
+	return a;
+}
+
+int main()
+{
+	int t{};
+	int n{}, m{};
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		cin >> n >> m;
+		cout<<Solution(n, m) << endl;
+	}
+}
