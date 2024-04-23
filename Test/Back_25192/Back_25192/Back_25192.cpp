ee4f67f847cbeec7bcbba2f80ffbe1a1d753a2b2
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-23 15:19:53 +0900

    인사성 밝은 곰곰이

---
+++ Test/Back_25192/Back_25192/Back_25192.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+set<string> Set;
+
+int main()
+{
+	int n{};
+	string s{};
+	int length{};
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> s;
+		if(s == "ENTER")
+		{
+			length += (int)Set.size();
+			Set.clear();
+			continue;
+		}
+		Set.insert(s);
+	}
+	length += (int)Set.size();
+	cout << length << endl;
+}
