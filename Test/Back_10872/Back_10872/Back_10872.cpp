d7ea780a9d53b2c1ce80f0a776849b24110a618a
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-23 13:57:10 +0900

    펙토리얼

---
+++ Test/Back_10872/Back_10872/Back_10872.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n{};
+	int result = 1;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		result *= i;
+	}
+
+	cout << result << endl;
+}
