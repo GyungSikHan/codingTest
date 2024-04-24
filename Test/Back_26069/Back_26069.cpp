76765b8d6b38bf1ca026212c1c11fa51839ec8ac
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-24 14:07:18 +0900

    붙임성 좋은 총총이

---
+++ Test/Back_26069/Back_26069.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+
+map<string, bool> name;
+
+int main()
+{
+	int n{};
+	cin >> n;
+	string s1{}, s2{};
+
+	while (n--)
+	{
+		cin >> s1 >> s2;
+
+		if (s1 == "chongchong")
+			name.insert({ s1, true });
+		else if (s2 == "chongchong")
+			name.insert({ s2, true });
+
+		if (name[s1] == true)
+			name[s2] = true;
+		if (name[s2] == true)
+			name[s1] = true;
+	}
+	int count{};
+	for (auto iter : name)
+		if (iter.second == true)
+			count++;
+
+	cout << count << endl;
+}
