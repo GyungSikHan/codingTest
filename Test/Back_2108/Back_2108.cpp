53e80fe8fc0b5c017bfd582bd4ab2f8d2876bdd7
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-24 16:35:15 +0900

    통계학

---
+++ Test/Back_2108/Back_2108.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+vector<int> datas;
+int n;
+
+int main()
+{
+	int temp{}, range{}, middle{}, mostv{}, mean{}, most = -9999;
+	int number[8001]{};
+	bool notFirst{};
+
+	cin >> n;
+
+	datas.resize(n, 0);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> temp;
+		datas[i] = temp;
+		mean += temp;
+		number[temp + 4000]++;
+	}
+
+	sort(datas.begin(), datas.end());
+	for (int i = 0; i < 8001; i++)
+	{
+		if (number[i] == 0)
+			continue;
+		if (number[i] == most)
+		{
+			if (notFirst == true)
+			{
+				mostv = i - 4000;
+				notFirst = false;
+			}
+		}
+		if (number[i] > most)
+		{
+			most = number[i];
+			mostv = i - 4000;
+			notFirst = true;
+		}
+	}
+
+	mean = round((float)mean / n);
+	middle = datas[datas.size() / 2];
+	range = datas.back() - datas.front();
+
+	cout << mean << endl;
+	cout << middle << endl;
+	cout << mostv << endl;
+	cout << range << endl;
+}
