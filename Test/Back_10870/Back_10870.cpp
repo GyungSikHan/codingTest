acbce08b206ebbd3e49d138c466edbcc6397c8d0
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-25 14:04:09 +0900

    피보나치 수 5

---
+++ Test/Back_10870/Back_10870.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int arr[21];
+
+void Pivo(int n)
+{
+	for (int i = 2; i <= n; i++)
+		arr[i] = arr[i - 1] + arr[i - 2];
+}
+
+int main()
+{
+	int n{};
+	cin >> n;
+
+	arr[0] = 0;
+	arr[1] = 1;
+
+	Pivo(n);
+
+	cout << arr[n] << endl;
+}
