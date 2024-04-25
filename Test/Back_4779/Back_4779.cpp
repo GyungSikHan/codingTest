267010b41ced5e47b5eee54d895c725a5e2cc2a8
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-25 18:02:11 +0900

    칸토어 집합

---
+++ Test/Back_4779/Back_4779.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+// 칸토어 집합 생성 함수
+string cantorSet(int n, int start, int end) {
+    if (n == 0) {
+        return "-";
+    }
+
+    int length = end - start;
+    int interval = length / 3;
+
+    string result = cantorSet(n - 1, start, start + interval);
+    result += string(interval, ' ');
+    result += cantorSet(n - 1, end - interval, end);
+
+    return result;
+}
+
+int main() {
+    int n;
+    while (cin >> n) 
+    {
+        int size = pow(3, n);
+        string cantor = cantorSet(n, 0, size);
+
+        cout << cantor << endl;
+    }
+
+    return 0;
+}
