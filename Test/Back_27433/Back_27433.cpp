1f756dd97afc0e425219a2686be6d6e6a261ab89
GyungSikHan <gksrudtlr2@icloud.com>
2024-04-25 13:57:14 +0900

    팩토리얼

---
+++ Test/Back_27433/Back_27433.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long result = 1;
+
+void Factorial(int n)
+{
+	if (n == 0)
+		return;
+	result *= n;
+	Factorial(n - 1);
+}
+
+int main()
+{
+	int n{  };
+	cin >> n;
+
+	Factorial(n);
+
+	cout << result << endl;
+}
