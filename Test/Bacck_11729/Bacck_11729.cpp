8044ac0b3313349650ea1c9571367671a146e2ba
GyungSikHan <gksrudtlr2@icloud.com>
2024-05-23 18:50:07 +0900

    하노이탑

---
+++ Test/Bacck_11729/Bacck_11729.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void Hanoi(int n, int start, int end, int aux)
+{
+	if(n == 1)
+	{
+		cout << start << " " << end << "\n";
+		return;
+	}
+
+	Hanoi(n - 1, start, aux, end);
+	cout << start << " " << end << "\n";
+	Hanoi(n - 1, aux, end, start);
+}
+
+int main()
+{
+	int n{};
+	cin >> n;
+
+	std::cout << (1 << n) - 1 << '\n';
+	//cout << pow(2, n) - 1 << endl; // 시간초과남
+
+	Hanoi(n, 1, 3, 2);
+	
+}
