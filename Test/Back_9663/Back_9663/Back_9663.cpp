6edbab8b2c05859367ae3cfa319bab3758017066
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2024-07-07 20:03:01 +0900

    N-Queen

---
+++ Test/Back_9663/Back_9663/Back_9663.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n, total=0;
+int col[15];
+
+bool Check(int level)
+{
+	for (int i = 0; i < level; i++)
+		if (col[i] == col[level] || abs(col[level] - col[i]) == level - i)
+			return false;
+	
+	return true;
+}
+
+
+void NQueen(int x)
+{
+	if (x == n)
+		total++;
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			col[x] = i;
+			if (Check(x) == true)
+				NQueen(x + 1);
+		}
+	}
+}
+
+int main()
+{
+	cin >> n;
+
+	NQueen(0);
+	cout << total << endl;
+}
