517edfbaf6de9e27c87cee3bc34ea1be5590138a
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2024-07-30 18:04:38 +0900

    재귀로 푼거 포함

--- Study/1Weeks/16_Beak_1940.cpp
+++ Study/1Weeks/16_Beak_1940.cpp
@@ -20,3 +20,27 @@ int main()
           Count++;
     cout << Count << endl;
 }
+
+////재귀함수
+// void combi(int idx, vector<int> & v){
+//     if(v.size() == 2){ 
+//         int b = a[v[0]];
+//         int c = a[v[1]];
+//         if(b + c == m)cnt++;
+//         return;
+//     }
+//     for(int i = idx + 1; i < n; i++){
+//         v.push_back(i);
+//         combi(i, v);
+//         v.pop_back();
+//     }
+// }
+// int main() {
+// 	ios_base::sync_with_stdio(false);
+// 	cin.tie(NULL);
+// 	cin >> n >> m; 
+// 	for(int i = 0; i < n; i++) cin >> a[i];   
+//     vector<int> v; 
+//     combi(-1, v);    
+//     cout << cnt << '\n';
+// }
