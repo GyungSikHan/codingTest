45b21cafb98b87d0064281c932c5553115cb3c92
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2024-08-07 15:31:11 +0900

    Baek 3986 좋은 단어

---
+++ Study/1Weeks/15_Baek_3986.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int n;
+int Count;
+string S;
+
+int main()
+{
+    cin>>n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin>>S;
+        stack<char> s{};
+        for (char c : S) 
+        {           
+            if(s.empty() == false && s.top() == c)
+                s.pop();
+            else
+                s.push(c);
+        }
+        if(s.empty() == true)
+            Count++;
+    }    
+
+    cout<<Count<<endl;
+}
