c9f075e7a1d2edaf7ba0a3f254dc72f88d3f14a5
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2024-08-19 17:08:05 +0900

    종화는 방구쟁이 예제

---
+++ Study/2Weeks/06_JungWhoaIsBullshit.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+int dY[4] = {-1,0,1,0};
+int dX[4] = {0,1,0,-1};
+
+int Count;
+int Count2;
+
+void DFS(vector<vector<int>> maps, vector<vector<bool>>& Visited, int y, int x)
+{
+    Visited[y][x] = true;
+
+    for (int i = 0; i < 4; i++) 
+    {
+        int nY = y+dY[i];
+        int nX = x+dX[i];
+        if(nY >= 0 && nY < n && nX >= 0 && nX < n)
+            if(maps[nY][nX] == 1 && Visited[nY][nX] == false)
+                DFS(maps, Visited, nY, nX);
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    vector<vector<int>> maps(n,vector<int>(m, 0));
+    vector<vector<bool>> Visited(n, vector<bool>(m,false));
+    
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < m; j++) 
+            cin>>maps[i][j];
+    
+    
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j < m; j++) 
+        {
+            if(maps[i][j] == 1 && Visited[i][j] == false)
+            {
+                DFS(maps, Visited, i, j);
+                Count++;
+            }
+        }
+    }
+
+    cout<<Count<<endl;
+}
