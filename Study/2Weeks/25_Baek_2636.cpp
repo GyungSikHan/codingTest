47332b77a88fd2e9d8c7952624e253e1661b07c4
yerinHanHH <156291511+yerinHanHH@users.noreply.github.com>
2024-10-16 20:57:56 +0900

    2636 치즈

---
+++ Study/2Weeks/25_Baek_2636.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int nX[4] = {-1,0,1,0};
+int nY[4] = {0,1,0,-1};
+
+int n, m;
+int Time;
+int Chees;
+int Map[100][100];
+int Visited[100][100];
+vector<pair<int, int>> vP;
+
+void DFS(int x, int y)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            
+        }
+        
+    }
+    
+}
+
+int main()
+{
+    cin>>n>>m;
+   
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin>>Map[i][j];
+
+    while (true)
+    {
+        /* code */
+    }
+    
+     
+
+    
+}
