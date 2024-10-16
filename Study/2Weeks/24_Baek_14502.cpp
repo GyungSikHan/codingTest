0b010bb56840c6b3d106e4720d2ab374d835d101
yerinHanHH <156291511+yerinHanHH@users.noreply.github.com>
2024-10-16 19:55:17 +0900

    14502 연구소

--- Study/2Weeks/24_Baek_14502.cpp
+++ Study/2Weeks/24_Baek_14502.cpp
@@ -1,17 +1,79 @@
 #include <iostream>
 #include <vector>
-#include <string.h>
+
 using namespace std;
 
-int n, m;
-int arr[8][8];
+int n,m;
+int Arr[8][8];
 int Temp[8][8];
-int count;
-int result;
-vector<pair<int,int>> v;
+vector<pair<int, int>> V;
+
+int Result;
+
+void DFS(int x, int y)
+{
+    Temp[x][y] = 2;
+    if(x-1>=0 && Arr[x-1][y] == 0 && Temp[x-1][y] == 0)
+        DFS(x-1,y);
+    if(x+1<n && Arr[x+1][y] == 0 && Temp[x+1][y] == 0)
+        DFS(x+1,y);
+    if(y-1>=0 && Arr[x][y-1] == 0 && Temp[x][y-1] == 0)
+        DFS(x,y-1);
+    if(y+1<n && Arr[x][y+1] == 0 && Temp[x][y+1] == 0)
+        DFS(x,y+1);
+}
+
+void Wall(int data)
+{
+    if(Count == 3)
+    {
+        memset(Temp, 0, sizeof(Temp));
+        for (int i = 0; i < V.size(); i++)
+        {
+            DFS(V[i].first, V[i].second);    
+        }
+        Count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if(Arr[i][j] == 0 && Temp[i][j] == 0)
+                    data++;
+            }
+        }
+        Result = max(Result, data);
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if(Arr[i][j] == 0)
+                {
+                    Arr[i][j] = 1;
+                    Wall(data + 1);
+                    Arr[i][j] = 0;
+                }
+            }
+            
+        }
+        
+    }
+}
 
 int main()
 {
     cin >> n >> m;
-    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> Arr[i][j];
+            if(Arr[i][j] == 2)
+                V.push_back(make_pair(i,j));
+        }
+    }
+    Wall(0);
+    cout<<Result<<endl;
 }
