0d81c0304e8dfcdd6755ac641b97ec79021eefe5
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2024-10-16 17:19:54 +0900

    14502 연구소

---
+++ Study/2Weeks/24_Baek_14502.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+using namespace std;
+
+int n, m;
+int arr[8][8];
+int Temp[8][8];
+int count;
+int result;
+vector<pair<int,int>> v;
+
+int main()
+{
+    cin >> n >> m;
+    
+}
