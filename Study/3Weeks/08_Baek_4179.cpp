bda9f4cd15c88ec23e9ce5c8235cde8912d4da6c
yerinHanHH <156291511+yerinHanHH@users.noreply.github.com>
2024-10-28 20:42:46 +0900

    4179

--- Study/3Weeks/08_Baek_4179.cpp
+++ Study/3Weeks/08_Baek_4179.cpp
@@ -1,53 +1,63 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 using namespace std;
 
-int dX[4] = {-1,0,1,0}, dY[4] = {0,1,0,-1};
-vector<vector<char>> Map;
-vector<vector<bool>> Visited;
-int n,m;
-char c;
-int Count;
-bool b;
-
-void DFS(int x, int y)
-{
-    if(x < 0 || x >= n || y < 0 || y >= m)
-    {
-        b = true;
-        return;
-    }
-
-    for (int i = 0; i < 4; i++)
-    {
-        int nX = x + dX[i];
-        int nY = y +dY[i];
-        if(Map[nX][nY] == 'F' || Map[nX][nY] == '#')
-            continue;
-        if(Visited[nX][nY] == false)
-        {
-            Visited[nX][nY] = true;
-            Count++;
-            DFS(nX, nY);
-            Visited[nX][nY] = false;
-            Count--;
-        }
-    }
-    
-}
+int dX[4] = {-1, 0, 1, 0}, dY[4] = {0, 1, 0, -1};
+int n, m;
+int x, y;
+char Map[1001][1001];
+bool JeHawn[1001][1001];
+bool Fire[1001][1001];
+queue<pair<int, int>> q;
 
 int main()
 {
-    cin>>n>>m;
-    Map.resize(n,vector<char>(m));
-    Visited.resize(n,vector<bool>(m));
+    cin >> n >> m;
     for (int i = 0; i < n; i++)
+    {
         for (int j = 0; j < m; j++)
-            cin>>Map[i][j];
-        
-    DFS(0,0);
-    if(b == false)
-        cout<<"IMPOSSIBLE"<<"\n";
-    else
-        cout<<Count<<"\n";
+        {
+            cin >> Map[i][j];
+            if (Map[i][j] == 'F')
+            {
+                Fire[i][j] = true;
+                q.push({i, j});
+            }
+            else if (Map[i][j] == 'J')
+            {
+                x = i;
+                y = j;
+            }
+        }
+    }
+
+    while (q.empty() == false)
+    {
+        int X = q.front().first;
+        int Y = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nX = X + dX[i];
+            int nY = Y + dY[i];
+            if (nX < 0 || nX >= n || nY < 0 || nY >= m)
+                continue;
+            if (Fire[nX][nY] == true || Map[nX][nY] == '#')
+                continue;
+            Fire[nX][nY] = true;
+            q.push({nX, nY});
+        }
+    }
+
+    JeHawn[x][y] = true;
+    q.push({x,y});
+    while (q.empty() == false)
+    {
+        int nX = q.front().first;
+        int nY = q.front().second;
+        q.pop();
+        if(nX == n-1 || nX == n|| nY == m-1)
+    }
+    
 }
