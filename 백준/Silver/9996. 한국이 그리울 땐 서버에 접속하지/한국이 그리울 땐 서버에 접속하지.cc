4ff205bbd5c3fce84c164a32963da363fc024388
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2024-12-24 15:22:21 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n{};
+string s{}, ori_s, pre, suf;
+
+int main()
+{
+    cin>>n;
+    cin>>ori_s;
+    int pos = ori_s.find('*');
+    pre=ori_s.substr(0,pos);
+    suf=ori_s.substr(pos+1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin>>s;
+        if(pre.size() + suf.size() > s.size())
+            cout<<"NE"<<endl;
+        else
+        {
+            if(pre == s.substr(0,pre.size()) && suf == s.substr(s.size() - suf.size()))
+                cout<<"DA"<<endl;
+            else
+                cout<<"NE"<<endl;
+        }
+    }
+    
+}
